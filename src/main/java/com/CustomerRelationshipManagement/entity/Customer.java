ce8eade05f0ad5842f9ce5a2d03bf77fd9481fbd
mindedboys <rohitvrm888@gmail.com>
2024-04-08 17:56:12 +0530

    just add comment is model class

--- src/main/java/com/CustomerRelationshipManagement/entity/Customer.java
+++ src/main/java/com/CustomerRelationshipManagement/entity/Customer.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//This model class entity used CRM
 
 @Entity
 @Table(name="customers")
